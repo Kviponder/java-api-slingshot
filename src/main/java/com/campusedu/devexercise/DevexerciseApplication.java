34cd7b44c8d9448a3cca424325248f85dc8b7972
Kviponder <119442089+Kviponder@users.noreply.github.com>
2023-10-29 14:43:05 -0400

    Tring to figure out DDL issues

--- src/main/java/com/campusedu/devexercise/DevexerciseApplication.java
+++ src/main/java/com/campusedu/devexercise/DevexerciseApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DevexerciseApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(DevexerciseApplication.class, args);
 	}
 
