897f9a6791a861229456b4085d05b47cbbcd2840
Kviponder <119442089+Kviponder@users.noreply.github.com>
2023-11-01 19:29:19 -0400

    Added service layer logic, starter controller log

--- src/main/java/com/campusedu/devexercise/controllers/CourseInfoController.java
+++ src/main/java/com/campusedu/devexercise/controllers/CourseInfoController.java
@@ -1,4 +1,22 @@
 package com.campusedu.devexercise.controllers;
 
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.campusedu.devexercise.model.Course_Info;
+import com.campusedu.devexercise.services.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/courseInfo")
 public class CourseInfoController {
+
+    private final CourseService courseService;
+
+    @Autowired 
+
 }
