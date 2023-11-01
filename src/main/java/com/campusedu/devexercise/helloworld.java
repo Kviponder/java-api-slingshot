b1624f06ab667eb14feba839010d39b06d617852
Kviponder <119442089+Kviponder@users.noreply.github.com>
2023-11-01 16:24:34 -0400

    Test commit

--- src/main/java/com/campusedu/devexercise/helloworld.java
+++ src/main/java/com/campusedu/devexercise/helloworld.java
@@ -2,6 +2,6 @@ package com.campusedu.devexercise;
 
 public class helloworld {
     public static void main(String[] args){
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World Test Commit!");
     }
 }
