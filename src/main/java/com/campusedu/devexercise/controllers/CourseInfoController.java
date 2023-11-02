566443777e8a9fdc2430af7ed7c40944dc2980a8
Kviponder <119442089+Kviponder@users.noreply.github.com>
2023-11-01 20:30:59 -0400

    Finished basic files, need to add logs to determine why app shuts down immediately after starting, finished controllers and added more exceptions

--- src/main/java/com/campusedu/devexercise/controllers/CourseInfoController.java
+++ src/main/java/com/campusedu/devexercise/controllers/CourseInfoController.java
@@ -1,11 +1,14 @@
 package com.campusedu.devexercise.controllers;
 
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.campusedu.devexercise.model.Course_Info;
 import com.campusedu.devexercise.services.CourseService;
+import com.campusedu.devexercise.exceptions.CourseNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,6 +20,23 @@ public class CourseInfoController {
 
     private final CourseService courseService;
 
-    @Autowired 
+    @Autowired
+    public CourseInfoController(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Course_Info>> getCourseInfo(@RequestParam(required = false) String courseCodePrefix,
+                                                          @RequestParam(required = false) String courseCodeNumber) {
+        try {
+            List<Course_Info> courseInfos = courseService.findByCourseCodePrefixAndCourseCodeNumber(courseCodePrefix, courseCodeNumber);
+            return ResponseEntity.ok(courseInfos);
+        } catch (CourseNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
 
 }
